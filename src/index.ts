2a8052ea281f177c5fd324983dfed0fd13ee03f5
Thanat <bztzn512@gmail.com>
2026-03-18 12:56:09 -0400

    fix backend links

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://frontend-taskapp-re-create.onrender.com/",
+    origin: "https://frontend-taskapp-re-create.onrender.com",
     credentials: true,
   }),
 );
